67abdf1f5487d89160ae1a118be72a33f49dddd4
flynn <flynn@BackupServer>
2024-07-06 00:10:33 +0200

    lightbulb

--- app/app.py
+++ app/app.py
@@ -4,17 +4,21 @@ import uvicorn
 from routers import pages
 
 app = FastAPI()
-
-# Mount static files
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/images", StaticFiles(directory="images"), name="images")
-app.mount("/videos", StaticFiles(directory="videos"), name="videos")
-
-# Include routers
 app.include_router(pages.router)
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    mount()
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+def mount():
+    # Include routers
+    
+    # Mount static files
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    app.mount("/images", StaticFiles(directory="images"), name="images")
+    app.mount("/videos", StaticFiles(directory="videos"), name="videos")
+
 
 if __name__ == "__main__":
     main()
